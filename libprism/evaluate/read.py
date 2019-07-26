5c64a2f8ac1bd12eb2cc46bedbf28165a648d29b
Yu Lin <yulin@bombus.anu.edu.au>
2019-07-26 11:19:07 +1000

    add read_sam in read

--- libprism/evaluate/read.py
+++ libprism/evaluate/read.py
@@ -5,33 +5,186 @@
 # Created Time: Thu 30 May 2019 15:34:53 AEST
 #########################################################################
 #!/bin/bash
+import tools
+
+def read_snp_sam(filename):
+    #bothHappen, oneHappen, zeroHappen = 0, 0, 0
+    bothHappen, oneHappen, zeroHappen = [], [], []
+    count = 0
+    align_0, align_1 = "", "" #alt_align_0, alt_align_1, = "","","",""
+    with open(filename, "r") as f:
+        line = f.readline()
+        state = 0
+        while line:
+            if line.startswith("@"):
+                line = f.readline()
+                continue
+            assert line.startswith("kmer")
+            words = line.split()
+            if state == 0:
+                count +=1
+                state = 1
+                ID = words[0].split('_')[1]
+                kmer_0 = words[9]
+                refPos_0 = words[3]
+                if words[-1].startswith("MD:Z:"):
+                    align_0 = words[-1].split(":")[-1]  
+                elif words[-1].startswith("XA:Z:"):
+                    align_0 = words[-2].split(":")[-1]
+                    alt_align_0 = words[-1].split(":")[-1]
+                line = f.readline()
+            elif state == 1:
+                state = 2
+                kmer_1 = words[9]
+                refPos_1 = words[3]
+                if words[-1].startswith("MD:Z:"):
+                    align_1 = words[-1].split(":")[-1]
+                elif words[-1].startswith("XA:Z:"):
+                    align_1 = words[-2].split(":")[-1]
+                    alt_align_1 = words[-1].split(":")[-1]
+                line = f.readline()
+            if state == 2:
+                state = 0
+                if align_0 == "31" and align_1 == "31":
+                    print "both exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                    bothHappen.append( (ID, refPos_0, refPos_1, kmer_0, kmer_1) )
+                elif align_0 == "31":
+                    print "one exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                    oneHappend.append( (ID, refPos_0, kmer_0, kmer_1) )
+                elif align_1 == "31":
+                    print "one exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                    oneHappend.append( (ID, refPos_1, kmer_0, kmer_1) )
+                else:
+                    print "none exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                    zeroHappend.append( (ID, refPos_0, refPos_1, kmer_0, kmer_1) )
+
+                '''
+                if refPos_0 == refPos_1:
+                    if ( (align_0 == "31" and ( align_1.startswith("15") and align_1.endswith("15") ) ) or
+                         (align_1 == "31" and ( align_0.startswith("15") and align_0.endswith("15") ) ) ):
+                        TP.append((ID, kmer_0, kmer_1))
+                    else:
+                        print "align to same position, but not 31/15*15:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                else:        
+                    if align_0 == "31" and align_1 == "31":
+                        print "both exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                        bothHappen += 1
+                    elif align_0 == "31" or align_1 == "31":
+                        print "one exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                        oneHappen += 1
+                    elif align_0 != "31" and align_1 != "31":
+                        print "none exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                        zeroHappen += 1
+                    else:    
+                        print ID, align_0, align_1
+                '''    
+                    
+    print len(TP), zeroHappen, oneHappen, bothHappen
+    print len(TP) + zeroHappen + oneHappen + bothHappen, count
+
+        
+def read_indel_sam(filename):
+    bothHappen, oneHappen, zeroHappen = 0, 0, 0
+    TP = []
+    TP_refPos = []
+    count = 0
+    indelCount = 0
+    align_0, align_1, alt_align_0, alt_align_1, = "","","",""
+    with open(filename, "r") as f:
+        line = f.readline()
+        state = 0
+        while line:
+            if line.startswith("@"):
+                line = f.readline()
+                continue
+            assert line.startswith("kmer")
+            words = line.split()
+            if state == 0:
+                count +=1
+                state = 1
+                ID = words[0].split('_')[1]
+                kmer_0 = words[9]
+                refPos_0 = words[3]
+                if words[-1].startswith("MD:Z:"):
+                    align_0 = words[-1].split(":")[-1]  
+                elif words[-1].startswith("XA:Z:"):
+                    align_0 = words[-2].split(":")[-1]
+                    alt_align_0 = words[-1].split(":")[-1]
+                line = f.readline()
+            elif state == 1:
+                state = 2
+                kmer_1 = words[9]
+                refPos_1 = words[3]
+                if words[-1].startswith("MD:Z:"):
+                    align_1 = words[-1].split(":")[-1]
+                elif words[-1].startswith("XA:Z:"):
+                    align_1 = words[-2].split(":")[-1]
+                    alt_align_1 = words[-1].split(":")[-1]
+                line = f.readline()
+            if state == 2:
+                state = 0
+                if refPos_0 == refPos_1:
+                        TP.append((ID, kmer_0, kmer_1))
+                        TP_refPos.append(int(refPos_0))
+                        assert len(kmer_0) == 31 
+                        midCnt = tools.count_mid_same(kmer_0)
+                        indelCount += 1.0/midCnt  
+                else:        
+                    print "FP:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                    print "alt align", alt_align_0
+                    print "alt align", alt_align_1
+                    '''
+                    bothHappen += 1
+                    elif align_0 == "31" or align_1 == "31":
+                        print "one exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                        oneHappen += 1
+                    elif align_0 != "31" and align_1 != "31":
+                        print "none exactly happen:", ID, align_0, align_1, "refpos:", refPos_0, refPos_1
+                        print "alt align", alt_align_0
+                        print "alt align", alt_align_1
+                        zeroHappen += 1
+                    else:    
+                        print ID, align_0, align_1
+                    '''    
+                    
+    print "TP number:", len(TP)
+    print "FP number:", count-len(TP)
+    print "TP indel number:", indelCount
 
 
 def read_vcf(filename):
     f = open(filename, "r")
-    snps = {}
+    mutations = {}
     ID = 1
     for line in f:
         if line.startswith('#'):
             continue
         words = line.split()
         pos = int(words[1])
-        #print (pos)
         s1 = words[3].strip()
         s2 = words[4].strip()
         homo = words[9].split(':')[0]
-        #print homo
         assert homo == "1/0" or homo == "1|0" or homo == "0|1" or homo == "0/1"
         s1Len = len(s1)
         s2Len = len(s2)
-        assert s1Len == 1 and s2Len == 1
+        #assert s1Len == 1 and s2Len == 1
         if s1 == '.' or s2 == '.':
             continue
-        snps[pos] = (s1, s2, ID)
+        mutations[pos] = (s1, s2, ID)
         ID += 1
 
     f.close()     
-    return snps    
+    return mutations    
 
 
 def read_group_kmer(filename):
