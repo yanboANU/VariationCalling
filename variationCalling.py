110c3c93fa5aba754ee7d4d27a6e208d9e5eadb5
Yanbo <yanbo.li@anu.edu.au>
2019-10-01 09:14:47 +1000

    updata variationCalling.py: add one para, extend 1 range

--- variationCalling.py
+++ variationCalling.py
@@ -8,12 +8,13 @@
 import argparse
 import os
 import sys
-from libprism.local import kmercalling
+from libprismv2.local import kmercalling
 #from libprism.local.prepare import clouds_from_refhap, merge_clouds, print_clouds
 from libprism.evaluate import read
 #from math import log, exp
 #from pysam
 import logging
+import time
 
 #############################################################################
 
@@ -26,43 +27,67 @@ parser.add_argument('--t2', help='k-1mer freq txt file (dsk result)', required=T
 parser.add_argument('--c1', help='low coverage', required=True)
 parser.add_argument('--c2', help='high coverage', required=True)
 parser.add_argument('--k', help='kmer size', required=True)
+parser.add_argument('--b', help='for simulate data 0, real data 1, can dicide indel threshold', required=True)
 #parser.add_argument('--o', help='output file prefix', required=True)
 
 args = parser.parse_args()
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 #############################################################################
-
+time1 = time.clock()
 lowCov, highCov = int(args.c1), int(args.c2)
+
+## try to filter more erronoure kmer
+#if lowCov < 10:
+#    lowCov = 10
+
+# try this
+lowCov = lowCov - 1
+highCov = highCov + 1
+
 k = int(args.k)
-'''
-uniqKmer = kmercalling.pick_smaller_unique_kmer( args.t1, 
+
+kmerCov = kmercalling.pick_smaller_unique_kmer( args.t1, 
+             lowCov, highCov)
+k_1merCov = kmercalling.pick_smaller_unique_kmer( args.t2, 
              lowCov, highCov)
 
-uniqK_1mer = kmercalling.pick_smaller_unique_kmer( args.t2, 
-             lowCov, highCov)
-'''             
-
+time2 = time.clock()
 # uniq.kmer
-uniqKmer = read.read_multip_columns(args.t1) 
-uniqK_1mer = read.read_multip_columns(args.t2)
+#uniqKmer = read.read_multip_columns(args.t1) 
+#uniqK_1mer = read.read_multip_columns(args.t2)
+logging.info( "uniq Kmer size: %s" % len(kmerCov) )
+logging.info( "uniq (K-1)mer size: %s" % len(k_1merCov) )
+logging.info( "finish reading (kmer cov) and (k-1mer cov) file, cost %s seconds" % (time2-time1))
+left_index, right_index = kmercalling.build_left_right_kmer_index(kmerCov)
 
-logging.info( "finish reading (kmer cov) and (k-1mer cov) file" )
-
-mapK, snpPair = kmercalling.find_snp_pair_kmer(uniqKmer, k)
-
-logging.info( "finish find snp" )
-
-indelPair = []
-#indelPair = kmercalling.find_indel_pair_kmer(mapK, uniqK_1mer, k)
-#logging.info( "finish find indel" )
+mapK, snpPair, leftKmer = kmercalling.find_snp_pair_kmer(kmerCov, k, left_index, right_index)
+time3 = time.clock()
+logging.info( "finish find snp, cost %s second" % (time3 - time2) )
+logging.info( "finish find snp, leftKmer size %s" % len(leftKmer) )
 nonPair = []
-#nonPair = kmercalling.find_non_pair_kmer(uniqKmer, k)
-#logging.info( "finish find non snp" )
+#nonPair, highRepeat = kmercalling.find_non_pair_kmer(uniqKmer, k, left_index, right_index)
+nonPair, highRepeat, leftKmer = kmercalling.find_non_pair_kmer(leftKmer, kmerCov, k, left_index, right_index)
+time4 = time.clock()
+logging.info( "finish find non snp, cost %s second" % (time4 - time3) )
+logging.info( "finish find non snp, leftKmer size %s" % len(leftKmer) )
+#time4 = time.clock()
+#adjustKmer = read.read_multip_columns("chr22_k31.adjust.sort.txt")
+#highRepeat = set()
+#left_index, right_index = kmercalling.build_left_right_kmer_index(adjustKmer)
+indelPair = []
+if args.b == "0":
+    threshold = 0
+else:
+    threshold = int(k/4)
+indelPair = kmercalling.find_indel_pair_kmer(leftKmer, kmerCov, k_1merCov, k, left_index, right_index, threshold)#, highRepeat)
+time5 = time.clock()
+logging.info( "finish find indel, cost %s seconds" % (time5 - time4) )
+'''
 hetePairs = []
 f1 = "k_" + str(k)+"_pair.snp"
 fout = open(f1, "w")
-'''
+
 ID = 0
 for (k1, k2, c1, c2) in snpPair:
     fout.write(">kmer_snp%s_1_cov_%s\n" % (ID, c1))
@@ -80,7 +105,7 @@ for (k1, k2, c1, c2) in indelPair:
     fout.write("%s\n" % ( k2 ) )
     ID += 1
     hetePairs.add( (k1, k2, "indel"+str(ID) ) )
-'''
+
 
 ID = 0
 for eles in snpPair:
@@ -125,6 +150,7 @@ for (k1, k2, c1, c2, c3, c4) in nonPair:
 fout.close()
 
 #print (type(k))
+
 left_index, right_index = kmercalling.build_left_right_kmer_index(uniqKmer)
 extendPair = kmercalling.extend_pair(hetePairs, left_index, right_index, k)
 
@@ -137,4 +163,4 @@ with open(f2, "w") as fout:
             fout.write(">kmer_%s_%s\n" % (ID, cnt))
             fout.write("%s\n" % ( kmer ) )
             cnt += 1
-            
+'''            
